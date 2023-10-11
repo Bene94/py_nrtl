b78ba5c66ebfa8238f81aa748736e4fcb0c8be78
Benedikt <bewinter@ethz.ch>
2023-10-11 15:54:09 +0200

    changed test

--- test.py
+++ test.py
@@ -33,13 +33,13 @@ tau = np.array([
 ])
 
 alpha = np.array([
-    [0.0, -1.38],
-    [-1.38, 0.0]
+    [0.0, 0.125],
+    [0.125, 0.0]
 ])
 
 tau = np.array([
-    [0.0, 0.72],
-    [0.91, 0.0]
+    [0.0, 1.1278],
+    [1.0481, 0.0]
 ])
 
 #alpha = np.array([[0. , 0.23449744],
@@ -52,7 +52,7 @@ tau = np.array([
 #tau = np.array([[0. , 0.3 ],
 #                [0.3, 0. ]])
 
-z = np.array([0.5, 0.5])  # Mole fractions for the mixture
+z = np.array([0.5059, 0.4951])  # Mole fractions for the mixture
 x0 = np.array([0.0, 1])  # Initial guess for phase 1 mole fractions
 
 # Define your parameters here
@@ -70,19 +70,14 @@ for i in range(1000):
     x, y, __ = py_nrtl.calc_lle_py(alpha, tau, z, x0)
 time_end_rust = time.time()
 
+print(f"RUST x: {x} y: {y}")
+
 time_start_python = time.time()
 for i in range(1000):
     x, y, beta = calc_LLE(alpha, tau, z, x0)
 time_end_python = time.time()
 
-time_start_empty_loop = time.time()
-a = 0
-for i in range(1000):
-    dummy()
-print(a)
-time_end_empty_loop = time.time()
-
-
+print(f"Python x: {x} y: {y}")
 time_start_rust_par = time.time()
 x_par, y_par, beta_par = py_nrtl.calc_lle_par_py(alphas, taus, zs, x0s)
 time_end_rust_par = time.time()
@@ -91,7 +86,6 @@ time_end_rust_par = time.time()
 print("Runtimes:")
 print("Rust: ", np.round(time_end_rust - time_start_rust,3), "s")
 print("Python: ", np.round(time_end_python - time_start_python,2), "s")
-print("Empty Loop: ", np.round(time_end_empty_loop - time_start_empty_loop,2), "s")
 print("Speedup: ", np.round((time_end_python - time_start_python)/(time_end_rust - time_start_rust),2))
 
 
